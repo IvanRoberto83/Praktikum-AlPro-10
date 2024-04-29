b82d44d93020c5537efeaff933b9bc45f896496a
Ivan <ivan.roberto@ti.ukdw.ac.id>
2024-04-29 20:54:15 +0700

    Laporan Praktikum 10

--- Soal 3/soal3.py
+++ Soal 3/soal3.py
@@ -1,9 +1,19 @@
 handle = open('berita.txt')
+kata_unik = []
 
 print("===== ISI BERITA =====")
 for kata in handle:
-    print(kata)
-    kata_unik = kata.split()
+    print(kata.strip())
+    x = kata.split()
+
+    for kata in x:
+        i = kata.strip(",.?!-")
+
+        if i not in kata_unik:
+            if i != "":
+                if i.lower() not in kata_unik:
+                    kata_unik.append(i)
 
 print("\n===== KATA UNIK PADA BERITA =====")
 print(kata_unik)
+
